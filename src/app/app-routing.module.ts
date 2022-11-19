41b77428a57cc3e6b42611b635960092900e06ea
Diego Faria <diego.rdfaria@gmail.com>
2022-11-19 01:03:03 -0500

    Route products removed from authentication

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -20,7 +20,6 @@ const routes: Routes = [
       {
         path: '',
         component: ProductsPageComponent,
-        canActivate: [AuthenticationService]
       },
       {
         path: 'cart',
