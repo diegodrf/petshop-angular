743f5acb2995e9b44fba624eb20284dc3d2554a7
Diego Faria <diego.rdfaria@gmail.com>
2022-11-20 03:15:42 -0500

    Refactoring caritem

--- src/app/models/cart-item.ts
+++ src/app/models/cart-item.ts
@@ -1,7 +1,14 @@
-export interface CartItem {
-    id: string
-    product: string
-    quantity: string
-    price: number
-    image: string
+import { Product } from "./product.model";
+
+export class CartItem {
+    public id: string = (Math.random() * 10000).toString();
+    public price: number = this.product.price * this.quantity;
+    public image: string = this.product.images[0];
+
+    constructor(
+        public product: Product,
+        public quantity: number,
+    ) {
+
+    }
 }
