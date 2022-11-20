e9db6f78cc590b2d4bdb25bc0ef0bdc46a5c2ca1
Diego Faria <diego.rdfaria@gmail.com>
2022-11-20 00:57:48 -0500

    create the busines logic for reset password

--- src/app/pages/accounts/reset-password-page/reset-password-page.component.ts
+++ src/app/pages/accounts/reset-password-page/reset-password-page.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { catchError } from 'rxjs';
+import { DataService } from 'src/app/services/data.service';
 
 @Component({
   selector: 'app-reset-password-page',
@@ -7,9 +12,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ResetPasswordPageComponent implements OnInit {
 
-  constructor() { }
+  public busy: boolean = false;
+  public form: FormGroup;
+
+  public documentControl = ['', Validators.compose([
+    Validators.maxLength(11),
+    Validators.minLength(11),
+    Validators.required
+  ])]
+
+  constructor(
+    private dataService: DataService,
+    private fb: FormBuilder,
+    private toastr: ToastrService,
+    private router: Router
+  ) {
+    this.form = fb.group({
+      'document': this.documentControl
+    })
+  }
 
   ngOnInit(): void {
   }
 
+  submit() {
+    this.busy = true;
+    this.dataService
+      .resetPassword(this.form.controls['document'].value)
+      .pipe(
+        catchError((err: any) => {
+          console.error(err);
+          this.toastr.error(err);
+          this.busy = false;
+          return err;
+        })
+      )
+      .subscribe((data: any) => {
+        this.busy = false;
+        this.toastr.success(data.message, 'Senha restaurada.');
+        this.router.navigate(['/login']);
+      })
+  }
 }
