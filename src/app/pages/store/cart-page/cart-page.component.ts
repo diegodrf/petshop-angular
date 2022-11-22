f0d356a991dae5dabccdd91af4268041a9d1a88d
Diego Faria <diego.rdfaria@gmail.com>
2022-11-22 16:34:17 -0500

    Added busines logic to cart

--- src/app/pages/store/cart-page/cart-page.component.ts
+++ src/app/pages/store/cart-page/cart-page.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Cart } from 'src/app/models/cart';
+import { CartItem } from 'src/app/models/cart-item';
+import { CartService } from 'src/app/services/cart.service';
 
 @Component({
   selector: 'app-cart-page',
@@ -7,9 +10,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CartPageComponent implements OnInit {
 
-  constructor() { }
+  public cart!: Cart;
+
+  constructor(
+    private cartService: CartService
+  ) { }
 
   ngOnInit(): void {
+    this.loadCart();
+  }
+
+  private loadCart() {
+    this.cart = this.cartService.get();
+  }
+
+  public remoteItem(item: CartItem) {
+    this.cart.removeItem(item);
+    this.cartService.save(this.cart);
+  }
+
+  public clearCart() {
+    this.cartService.clear();
+    this.loadCart();
   }
 
 }
